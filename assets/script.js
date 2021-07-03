ad3d809c6f231f988e89a30f3dd2a9cb881a8f02
jhow04 <48718076+jhow04@users.noreply.github.com>
2021-07-03 18:23:40 +0800

    fix: heading

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,3 @@
-// $('.block img').on('mouseover', function(event){
-//     $('.block img').not($(this)).addClass('hover');
-// });
-
-// $('.block img').on('mouseout', function(){
-//     $('.block img').not($(this)).removeClass('hover');
-// });
-
-// $(window).on('resize load', function() {
-//     var win = $(this);
-//     if (win.width() <= 720) {
-//         $('.works').slick('unslick');
-//     } else {
-//         $('.works').slick({
-//             centerMode: true,
-//             centerPadding: '20px',
-//             slidesToShow: 1,
-//             dots: true,
-//             appendDots: $('.works-container').find('.dots'),
-//             arrows: false,
-//             infinite: false,
-//             mobileFirst: true,
-//             responsive: [
-//                 {
-//                 breakpoint: 1300 - 1,
-//                 settings: {
-//                         centerPadding: '180px',
-//                         slidesToShow: 1
-//                     }
-//                 },
-//                 {
-//                 breakpoint: 720,
-//                 settings: {
-//                         centerPadding: '80px',
-//                         slidesToShow: 1
-//                     }
-//                 }
-//             ]
-//         });
-//     }
-// });
-
 $('.works').slick({
     centerMode: true,
     centerPadding: '20px',
@@ -66,9 +24,6 @@ $('.works').slick({
         }
     ]
 });
-
-
-
 
 $('.works').on('afterChange', function(event, slick, currentSlide){
     $('.slick-current video').trigger('play');
